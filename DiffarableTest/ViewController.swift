8758fdabe24f75d9f827e3157030f164af90cf18
Михаил Назаров <msnazarow@gmail.com>
2022-06-19 12:06:27 +0300

    Initial Commit

---
+++ DiffarableTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiffarableTest
+//
+//  Created by Назаров Михаил Сергеевич on 19.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
